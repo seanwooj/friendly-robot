a8e396f0d4bccfe376b207452957d91d0f16c2e0
Sean Kim <seanwoojinkim@gmail.com>
2017-06-06 15:52:20 -0700

    can set colwidth within contentful

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,13 +3,30 @@ import ReactMarkdown from 'react-markdown';
 
 
 class Content extends Component {
+  state = {
+    colWidth: 'six'
+  }
+
   componentDidMount() {
+    const colWidth = this.props.content.fields.columnWidth;
+    if(colWidth) this.setState({colWidth: colWidth});
+  }
+
+  createClasses = () => {
+    let classes = ['content', 'columns'];
+
+    if(this.state.colWidth) {
+      classes.push(this.state.colWidth)
+    }
+
+    return classes;
   }
 
   render() {
+    const classes = this.createClasses();
 
     return(
-      <div className='content six columns'>
+      <div className={classes.join(' ')}>
         <ReactMarkdown source={this.props.content.fields.textContent} />
       </div>
     );
